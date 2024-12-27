b2b685ffd5bdd5b0cb0b704fe9e16f38d64cf3f7
aaradhyasingh2013 <kuki81kumari@gmail.com>
2024-12-27 17:36:33 +0530

    space complexity

---
+++ main.py
@@ -0,0 +1,5 @@
+def abc(a):
+    if a>=0:
+        return a*(a+1)/2
+print(abc(3))
+
---
+++ rain.py
@@ -0,0 +1,7 @@
+def array(c):
+    sum= 0
+    for i in c:
+        sum= sum+i
+    return sum
+c=[12,3,4,5]
+print(array(c))
